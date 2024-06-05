421dc2d7aff58fe4c64da17cf52cf46183c31506
jzhang38 <a1286225768@gmail.com>
2024-06-05 05:30:53 +0000

    add kaichen script

---
+++ train_scripts/kaichen_0.sh
@@ -0,0 +1,16 @@
+accelerate launch \
+--config_file  accelerate_configs/single_node.yaml \
+train.py \
+--batch-size 1 \
+--gradient-accumulate-every 4  \
+--seed 2024 \
+--output-dir  ./output/vicuna-7b-v1.5-extend \
+--wandb LMExtend \
+--max-train-steps 2000  \
+--learning-rate 1e-5  \
+--dataset PY007/slimpajama_Yi1.5_tokenized_upsample_4096_chunk_256K \
+--model 01-ai/Yi-1.5-9B-Chat-16K  \
+--seq-length 256000 \
+--rope-theta 1000000000 \
+--parallel_mode zigzag_ring_attn \
+--checkpointing-steps 200
