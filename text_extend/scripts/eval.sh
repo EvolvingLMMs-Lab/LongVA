92d95e307cefefea90c4a858e51407f30d54c32c
jzhang38 <a1286225768@gmail.com>
2024-06-16 13:09:04 +0800

    ok

--- text_extend/scripts/eval.sh
+++ text_extend/scripts/eval.sh
@@ -1,4 +1,4 @@
-for model in Qwen2-7B-Instruct-extend-step_1000 
+for model in Qwen2-7B-Instruct-extend-step_1000 Meta-Llama-3-8B-Instruct-extend-step1000
 do
 for num_distractor in 5
 do
@@ -9,7 +9,6 @@ accelerate launch --num_processes 8 --config_file  accelerate_configs/deepspeed_
     --context_interval   32000 \
     --depth_interval 0.1 \
     --num_samples 5 \
-    --prompt_template qwen2 \
     --rnd_number_digits 7 \
     --haystack_dir text_extend/PaulGrahamEssays \
     --num_distractor $num_distractor
@@ -18,35 +17,3 @@ done
 
 
 
-
-Meta-Llama-3-8B-Instruct-extend-step1000
-
-for model in LLaVA-NeXT-Qwen2-7B-extend-avgpool2x2-anyres7x7
-do
-for num_distractor in 0 5
-do
-accelerate launch --num_processes 8 --config_file  accelerate_configs/deepspeed_inference.yaml  --main_process_port 6000 text_extend/eval_text_niah.py \
-    --model text_extend/training_output/$model  \
-    --max_context_length 512000 \
-    --min_context_length 128000 \
-    --context_interval   128000 \
-    --depth_interval 0.2 \
-    --num_samples 1 \
-    --rnd_number_digits 7 \
-    --haystack_dir text_extend/PaulGrahamEssays \
-    --num_distractor $num_distractor
-done
-done
-
-
-
-
-
-
-
-
-# to plot the heatmap only
-accelerate launch --num_processes 1 --config_file  accelerate_configs/deepspeed_inference.yaml  --main_process_port 6000 text_extend/eval_text_niah.py \
-     --model text_extend/training_output/Qwen2-7B-Instruct-extend-step_1000  \
-     --num_distractor 0 \
-     --plot_only
--- text_extend/scripts/extend_qwen2.sh
+++ text_extend/scripts/extend_qwen2.sh
@@ -8,7 +8,7 @@ text_extend/text_extend_train.py \
 --wandb LMExtend \
 --max-train-steps 1000  \
 --learning-rate 1e-5  \
---dataset  PY007/slimpajama_Qwen2_tokenized_upsample_4096_chunk_224K \
+--dataset  PY007/slimpajama_Qwen2_tokenized_upsample_4096_chunk_256K \
 --model Qwen/Qwen2-7B-Instruct   \
 --seq-length 224000 \
 --rope-theta 1000000000 \
