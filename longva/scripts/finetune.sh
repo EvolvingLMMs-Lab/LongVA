eb50ed2899bef0feb1fe34967d4feff69a10a933
Kaichen Zhang - NTU <zhan0564@e.ntu.edu.sg>
2024-08-10 11:36:21 +0800

    Update finetune.sh

--- longva/scripts/finetune.sh
+++ longva/scripts/finetune.sh
@@ -6,7 +6,7 @@ export NCCL_DEBUG=INFO
 
 LLM_VERSION="Qwen/Qwen2-7B-Instruct"
 LLM_VERSION_CLEAN="${LLM_VERSION//\//_}"
-VISION_MODEL_VERSION="google/siglip-so400m-patch14-384"
+VISION_MODEL_VERSION="openai/clip-vit-large-patch14-336"
 VISION_MODEL_VERSION_CLEAN="${VISION_MODEL_VERSION//\//_}"
 
 ############### Pretrain ################
